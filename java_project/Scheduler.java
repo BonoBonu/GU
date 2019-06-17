a23a553c20cb5b55175bfdf1342c03bf56d448d0
BonoBonu <45268978+BonoBonu@users.noreply.github.com>
2019-06-18 02:01:32 +0900

    6/18 add

--- java_project/Scheduler.java
+++ java_project/Scheduler.java
@@ -4,17 +4,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Scheduler extends JFrame implements ActionListener {
 	
@@ -24,12 +29,14 @@ public class Scheduler extends JFrame implements ActionListener {
 	public static final int WIDTH =1400;
     public static final int HEIGHT =1500;
     
+    JTabbedPane tp=new JTabbedPane(); 
+    
 	public Scheduler(String g,String name,int w) {
-		Week set=new Week();
+		 Week set=new Week();
 		 setTitle(g);
 		 System.out.println(g);
          setSize(WIDTH, HEIGHT);	
-         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
          //setLayout(CardLayout());
          
          JPanel panel=new JPanel();
@@ -41,7 +48,6 @@ public class Scheduler extends JFrame implements ActionListener {
          nickname.setFont(new Font("굴림체", Font.BOLD, 50));
          panel_1.add(nickname);
          JButton button=new JButton(+w+" 주 계획표");
-         button.addActionListener(this);
          button.setFont(new Font("굴림체", Font.BOLD, 50));
          panel_1.add(button);
          panel.add(panel_1);
@@ -119,6 +125,7 @@ public class Scheduler extends JFrame implements ActionListener {
          today_plan.add(now);
          
          JButton complete=new JButton("완료");
+         complete.addActionListener(this);
          complete.setFont(new Font("굴림체", Font.BOLD, 30));
          today_plan.add(complete);
          
@@ -127,21 +134,115 @@ public class Scheduler extends JFrame implements ActionListener {
          plan.setEditable(false);
          plan.setFont(new Font("굴림체", Font.BOLD, 30));
          today_plan.add(plan);
-         
-        
-         
+              
          panel.add(today_plan);
-      
+         tp.add("main",panel);
+         tp.setFont(new Font("굴림체", Font.BOLD, 20));
          
          
+         JPanel set_panel=new JPanel();
+         set_panel.setLayout(null);
          
-         add(panel);
+         JPanel set_panel1=new JPanel();
+         set_panel1.setLayout(new FlowLayout());
+         JLabel everyday=new JLabel("planner scheduler  ");
+         everyday.setFont(new Font("굴림체", Font.BOLD, 30));
+         set_panel1.add(everyday);
+         
+         JComboBox comboBox=new JComboBox();
+         String[] week_n=new String[w+1];
+         for(int i=0;i<w;i++)
+        	 week_n[i]=(i+1)+" WEEK";
+         week_n[w]="every day";
+         comboBox.setModel(new DefaultComboBoxModel(week_n));
+         comboBox.setPreferredSize(new Dimension(180,50));
+         comboBox.setFont(new Font("굴림체", Font.BOLD, 30));
+         comboBox.setBackground(Color.WHITE);
+         set_panel1.add(comboBox);
+         
+         set_panel1.setBounds(200, 50,1000, 60);
+         set_panel.add(set_panel1);
+         
+         JPanel set_panel2=new JPanel();
+         set_panel2.setLayout(new FlowLayout());
+         JComboBox everyday_set=new JComboBox();
+         String[] t=new String[25];
+         for(int i=0;i<25;i++)
+        	 t[i]=i+" 시";
+         everyday_set.setModel(new DefaultComboBoxModel(t));
+         everyday_set.setPreferredSize(new Dimension(80,50));
+         everyday_set.setFont(new Font("굴림체", Font.BOLD, 20));
+         everyday_set.setBackground(Color.WHITE);
+         set_panel2.add(everyday_set);
+         
+         String[] m=new String[25];
+         for(int i=0;i<6;i++)
+        	 m[i]=(i*10)+"분";
+         JComboBox everyday_m=new JComboBox();
+         everyday_m.setModel(new DefaultComboBoxModel(m));
+         everyday_m.setPreferredSize(new Dimension(80,50));
+         everyday_m.setFont(new Font("굴림체", Font.BOLD, 20));
+         everyday_m.setBackground(Color.WHITE);
+         set_panel2.add(everyday_m);
+         
+         JLabel label_set=new JLabel("시작 ="
+         		+ "");
+         label_set.setFont(new Font("굴림체", Font.BOLD, 30));
+         set_panel2.add(label_set);
+         
+         JTextField T=new JTextField(2);
+         T.setBackground(Color.WHITE);
+         T.setFont(new Font("굴림체", Font.BOLD, 30));
+         set_panel2.add(T);
+         JLabel label_set1=new JLabel("시");
+         label_set1.setFont(new Font("굴림체", Font.BOLD, 30));
+         set_panel2.add(label_set1);
+         
+         JTextField M=new JTextField(2);
+         M.setBackground(Color.WHITE);
+         M.setFont(new Font("굴림체", Font.BOLD, 30));
+         set_panel2.add(M);
+         JLabel label_set2=new JLabel("분  동안 ");
+         label_set2.setFont(new Font("굴림체", Font.BOLD, 30));
+         set_panel2.add(label_set2);
+         
+         set_panel2.setBounds(200, 110,1000, 60);
+         set_panel.add(set_panel2);
+         
+         JPanel set_panel3=new JPanel();
+         
+         JTextArea set_plan=new JTextArea(3,60);
+         set_plan.setFont(new Font("굴림체", Font.BOLD, 30));
+         set_panel3.add(set_plan);
+         set_panel3.setBounds(100, 170,1200, 200);
+         set_panel.add(set_panel3);
+         
+         drawJPanel set_panel4=new drawJPanel();
+         set_panel4.setBounds(200, 370,1000, 1000);
+         set_panel.add(set_panel4);
+       
+         
+         
+         tp.add("setting",set_panel);
+         tp.setFont(new Font("굴림체", Font.BOLD, 20));
+         
+         
+         add(tp);
 	}
+	
 	
 	 public void actionPerformed(ActionEvent e){
 		 String what=e.getActionCommand();
-		 if(what.matches(".*계획표.*")) {
-			 make_schedule make=new make_schedule(what.charAt(0)-'0') ;
+		 if(what.matches(".*완료.*")) {
+			 
 		 }
 	 }
+}
+
+class drawJPanel extends JPanel{
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(Color.black);
+		g.drawOval(0,0,1000,1000);
+	}
 }
