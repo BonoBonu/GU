0750a3bf8b416f50b06120ef61a41cd9ae8959db
BonoBonu <45268978+BonoBonu@users.noreply.github.com>
2019-06-19 20:44:47 +0900

    Update room.java

--- java_project/room.java
+++ java_project/room.java
@@ -1,3 +1,4 @@
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -20,25 +21,29 @@ implements ActionListener{
 		 setTitle("My room");
          setSize(800, 800);	
          setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         setLayout(new GridLayout(2,1));
+         setLayout(null);
+       
          
          JButton game=new JButton("GAME");
          game.addActionListener(this);
          game.setFont(new Font("굴림체", Font.BOLD, 80));
+         game.setBounds(70, 80, 650, 200);
+         add(game);
          
          JButton scheduler=new JButton("SCHDULER");
          scheduler.addActionListener(this);
          scheduler.setFont(new Font("굴림체", Font.BOLD, 80));
-         add(game);
-         add(scheduler);
+         scheduler.setBounds(70, 400, 650, 200);
+         add( scheduler);
+         
          
 	}
 	
 	public void actionPerformed(ActionEvent e) {
 		String cho=e.getActionCommand( );
 		if(cho=="GAME") {
-			/*game_q gui = new game_q( );
-	        gui.setVisible(true);*/
+			game_q gui = new game_q( );
+	        gui.setVisible(true);
 		}
 		else {
 			 Week gui = new Week( );
